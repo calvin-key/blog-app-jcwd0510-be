6ffad4faa89b431153df6aaa546bf46e8588eca4
calvin-key <calvin.delanop7@gmail.com>
2024-12-10 11:07:52 +0700

    feat: reset-password (#4)

---
+++ src/lib/jwt.ts
@@ -0,0 +1,63 @@
+import { NextFunction, Request, Response } from "express";
+import { TokenExpiredError, verify } from "jsonwebtoken";
+import { JWT_SECRET, JWT_SECRET_FORGOT_PASSWORD } from "../config";
+
+export const verifyToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    res.status(401).send({
+      message: "Authentication failed, token is required",
+    });
+    return;
+  }
+
+  verify(token, JWT_SECRET!, (err, payload) => {
+    if (err) {
+      if (err instanceof TokenExpiredError) {
+        res.status(401).send({ message: "Token expired" });
+        return;
+      } else {
+        res.status(401).send({ message: "Invalid token" });
+        return;
+      }
+    }
+
+    res.locals.user = payload;
+
+    next();
+  });
+};
+
+export const verifyTokenReset = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    res.status(401).send({
+      message: "Authentication failed, token is required",
+    });
+    return;
+  }
+
+  verify(token, JWT_SECRET_FORGOT_PASSWORD!, (err, payload) => {
+    if (err) {
+      if (err instanceof TokenExpiredError) {
+        res.status(401).send({ message: "Token expired" });
+        return;
+      } else {
+        res.status(401).send({ message: "Invalid token" });
+        return;
+      }
+    }
+
+    res.locals.user = payload;
+
+    next();
+  });
+};
