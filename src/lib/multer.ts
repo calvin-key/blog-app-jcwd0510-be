1f625fefe41ed7ea76815a65e286ef62971dc231
calvin-key <calvin.delanop7@gmail.com>
2024-12-16 09:39:49 +0700

    feat: create blog

---
+++ src/lib/multer.ts
@@ -0,0 +1,9 @@
+import multer from "multer";
+
+export const uploader = (filelimit: number = 7) => {
+  const storage = multer.memoryStorage();
+
+  const limits = { fileSize: filelimit * 1024 * 1024 };
+
+  return multer({ storage, limits });
+};
